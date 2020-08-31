9be2cae11ea0e30e419d1603f7fc04395d8c3ba3
Jacob Short <jrshort89@gmail.com>
2020-08-31 23:07:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,9 +3,8 @@ def find_element_index(array, value_to_find)
   count = 0
   while array[count] do
   if array[count] == value_to_find
-    count
+    return count
   end
-  break
   count += 1
   end
 end
